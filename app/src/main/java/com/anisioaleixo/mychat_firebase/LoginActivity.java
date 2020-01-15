9c247bace3528df83ab9a76715710d7556fa1639
anisioaleixo <anisioaleixo@yahoo.com.br>
2020-01-15 20:12:25 -0300

    Ciração inicial das ações dos botões da tela de login!

--- app/src/main/java/com/anisioaleixo/mychat_firebase/LoginActivity.java
+++ app/src/main/java/com/anisioaleixo/mychat_firebase/LoginActivity.java
@@ -3,12 +3,40 @@ package com.anisioaleixo.mychat_firebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText senha;
+    private Button btnEntar;
+    private TextView txtAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.act_login);
+
+        email = findViewById(R.id.edt_email);
+        senha = findViewById(R.id.edt_password);
+        btnEntar = findViewById(R.id.btn_enter);
+        txtAccount = findViewById(R.id.txt_account);
+
+        btnEntar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Código do clique no botão entrar
+            }
+        });
+
+        txtAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               //Código do clique no Criar conta!
+            }
+        });
     }
 }
