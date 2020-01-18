a34c16840d26ed2a61f481d044cc6e53f22c7de4
anisioaleixo <anisioaleixo@yahoo.com.br>
2020-01-18 12:36:00 -0300

    Carregando imagem da media no imageview!

--- app/src/main/java/com/anisioaleixo/mychat_firebase/RegisterActivity.java
+++ app/src/main/java/com/anisioaleixo/mychat_firebase/RegisterActivity.java
@@ -1,9 +1,15 @@
 package com.anisioaleixo.mychat_firebase;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +23,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.io.IOException;
+
 public class RegisterActivity extends AppCompatActivity {
 
     private ImageView mImage;
@@ -24,6 +32,7 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText mEmail;
     private EditText mPassword;
     private Button mCadastrar;
+    private Uri mUriImagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +76,7 @@ public class RegisterActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         Toast.makeText(getApplicationContext(),"Cadastrado com sucesso!",Toast.LENGTH_LONG).show();
+                        saveUserInFirebase();
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
@@ -75,11 +85,32 @@ public class RegisterActivity extends AppCompatActivity {
                         Log.i("AATJ",e.getMessage());
                     }
                 });
+    }
 
+    private void saveUserInFirebase() {
 
     }
 
     private void alterarImage() {
-        Log.i("AATJ","Solicitando troca de imagem!");
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 0){
+            try {
+                mUriImagem = data.getData();
+                Bitmap bitmap = null;
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),mUriImagem);
+                mImage.setImageDrawable(new BitmapDrawable(bitmap));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
