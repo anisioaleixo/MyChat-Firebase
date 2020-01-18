6ea8783afaf13b6e93025de463f6f646c62d4ffa
anisioaleixo <anisioaleixo@yahoo.com.br>
2020-01-18 18:40:35 -0300

    Vincilando contatos no recicleview!

--- app/src/main/java/com/anisioaleixo/mychat_firebase/ContactsActivity.java
+++ app/src/main/java/com/anisioaleixo/mychat_firebase/ContactsActivity.java
@@ -1,27 +1,44 @@
 package com.anisioaleixo.mychat_firebase;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.squareup.picasso.Picasso;
+import com.xwray.groupie.GroupAdapter;
+import com.xwray.groupie.GroupieViewHolder;
+import com.xwray.groupie.Item;
 
 import java.util.List;
 
 public class ContactsActivity extends AppCompatActivity {
 
     private static final String TAG = "AATJ";
+    private GroupAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_contacts);
+
+        RecyclerView rv = findViewById(R.id.recycler);
+
+        adapter = new GroupAdapter();
+        rv.setAdapter(adapter);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+
         fechUser();
     }
 
@@ -30,15 +47,42 @@ public class ContactsActivity extends AppCompatActivity {
                 .addSnapshotListener(new EventListener<QuerySnapshot>() {
                     @Override
                     public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
-                        if (e !=null){
-                            Log.e(TAG,e.getMessage());
+                        if (e != null) {
+                            Log.e(TAG, e.getMessage());
                         }
                         List<DocumentSnapshot> docs = queryDocumentSnapshots.getDocuments();
-                        for (DocumentSnapshot doc : docs){
+                        for (DocumentSnapshot doc : docs) {
                             User user = doc.toObject(User.class);
-                            Log.d(TAG,user.getUserName());
+                            Log.d(TAG, user.getUserName());
+                            adapter.add(new UserItem(user));
                         }
                     }
                 });
+    }
+
+    private class UserItem extends Item<GroupieViewHolder> {
+
+        private final User user;
+
+        private UserItem(User user) {
+            this.user = user;
+        }
+
+        @Override
+        public void bind(@NonNull GroupieViewHolder viewHolder, int position) {
+            TextView txtUserName = viewHolder.itemView.findViewById(R.id.txtUserName);
+            ImageView imgUserFoto = viewHolder.itemView.findViewById(R.id.imgUserFoto);
+
+            txtUserName.setText(user.getUserName());
+
+            Picasso.get()
+                    .load(user.getProfileUrl())
+                    .into(imgUserFoto);
+        }
+
+        @Override
+        public int getLayout() {
+            return R.layout.item_user;
+        }
     }
 }
